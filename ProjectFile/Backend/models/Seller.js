7ad1afaebbb5f3e1f470ade2fc631cbffea0007d
srilakshmi426 <maddulasrilakshmi82@gmail.com>
2026-02-19 21:33:39 +0530

    Delete ProjectFile/Backend directory

--- ProjectFile/Backend/models/Seller.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const sellerSchema = new mongoose.Schema({
-  businessName: String,
-  email: String,
-  password: String,
-});
-
-module.exports = mongoose.model("Seller", sellerSchema);
